7b1d7b014f9ea1cf90cffa7c5f2675e9a02a44a1
bowm <1185961721@qq.com>
2021-02-01 11:02:22 +0800

    引入echarts

---
+++ backstage/src/views/chartTest.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div id="chart">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    import * as echarts from 'echarts';
+    export default {
+        name: "chartTest",
+        data(){
+            return {
+
+            }
+        },
+        mounted(){
+            this.init();
+        },
+        methods:{
+            init(){
+                var myChart = echarts.init(document.getElementById('chart'));
+                myChart.setOption({
+                    title: {
+                        text: 'ECharts 入门示例'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+#chart{
+    width: 500px;
+    height: 400px;
+}
+</style>
